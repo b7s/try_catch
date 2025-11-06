2e0b4683df9e638e004b8ef3cb18b96dbdd3b19c
Bruno <b7s@outlook.com>
2025-11-06 09:44:31 -0300

    Change try_catch return type to object
    Updated the try_catch function to return a structured object instead of an array, and modified the parameter name for the error closure.

--- tryCatchHelper.php
+++ tryCatchHelper.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Log;
 if(!function_exists('try_catch'))
 {
     /**
-     * Executes a callable and returns the result or error in a structured array.
+     * Executes a callable and returns the result or error in a structured object.
      *
-     * @param callable $callable
-     * @param bool $throwError
-     * @param bool $logErrors
+     * @param  callable  $callable
+     * @param  bool  $throwError
+     * @param  bool  $logErrors
+     * @param  Closure|null  $callbleOnError  Callable to execute on error
+     *
      * @return array{data: mixed, error: ?Throwable} Associative array containing 'data' with the result of the operation or null (failure), and 'error' with the caught exception or null (success)
      * @throws Throwable
      */
-    function try_catch(callable|Closure $callable, bool $throwError = false, bool $logErrors = true, ?Closure $onErrorClosure = null): object
+    function try_catch(callable|Closure $callable, bool $throwError = false, bool $logErrors = true, ?Closure $callbleOnError = null): object
     {
         $result = new class {
             public $data = null;
@@ -34,8 +36,8 @@ if(!function_exists('try_catch'))
 
             throw_if($throwError, fn() => throw $e);
 
-            if (is_callable($onErrorClosure)) {
-                $onErrorClosure();
+            if (is_callable($callbleOnError)) {
+                $callbleOnError();
             }
 
             $result->error = $e;
