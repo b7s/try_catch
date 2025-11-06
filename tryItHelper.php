107b911bef972f641138a1b96f5b247fe6f47069
Bruno <b7s@outlook.com>
2025-11-06 10:05:13 -0300

    Implement tryIt function for structured error handling
    Adds a tryIt function to execute callables with error handling.

---
+++ tryItHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+if(!function_exists('tryIt'))
+{
+    /**
+     * Executes a callable and returns the result or error in a structured object.
+     *
+     * @param  callable  $callable
+     * @param  bool  $throwError  It throws an error and will not return any value
+     * @param  bool  $logErrors  Write the error to the system log
+     * @param  Closure|null  $callbleOnError  Callable to execute on error
+     *
+     * @return array{data: mixed, error: ?Throwable} Object containing 'data' with the result of the operation or null (failure), and 'error' with the caught exception or null (success)
+     * @throws Throwable
+     */
+    function tryIt(callable|Closure $callable, bool $throwError = false, bool $logErrors = true, ?Closure $callbleOnError = null): object
+    {
+        $result = new class {
+            public $data = null;
+            public $error = null;
+        };
+
+        try {
+            $result->data = $callable();
+            return $result;
+        } catch (Throwable $e) {
+            if (is_callable($callbleOnError)) {
+                try {
+                    $callbleOnError();
+                } catch(Throwable $ce) {}
+            }
+
+            if ($logErrors) {
+                error_log($e);
+            }
+
+            if($throwError)
+                throw $e;
+
+            $result->error = $e;
+            return $result;
+        }
+    }
+}
